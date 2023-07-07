24d382df4b7c2695a31a429e788b18fe6a377fbf
Bishal487 <kunwar.bishal487@gmail.com>
2023-07-07 01:04:00 -0400

    first commit

---
+++ outputExamples.js
@@ -0,0 +1,7 @@
+// JavaScript Output defines the ways to display the output of a given code. The output can be displayed by using four different ways which are listed below:
+
+// 1. innerHTML: It is used to access an element. It defines the HTML content. 
+// Syntax: document.getElementById("id").innerHTML;
+
+// 2. document.write(): It is used for testing purpose. 
+// Syntax: document.write()
